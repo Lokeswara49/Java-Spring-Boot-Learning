ad8c3ad868fadfb731de2c35eff42417179f660c
Lokeswara49 <121753810+Lokeswara49@users.noreply.github.com>
2025-05-01 15:36:32 +0530

    Moved controller to demo folder

--- demo/src/main/java/com/example/demo/Controller/FunRestController.java
+++ demo/src/main/java/com/example/demo/Controller/FunRestController.java
@@ -1,14 +1,17 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/")
+//@RequestMapping("/api/")
 public class FunRestController {
-    @GetMapping("hello")
+    @Value("${coach}")
+    private String coach;
+    @GetMapping("/")
     public String sayHello(){
-        return "Hello World";
+        return coach;
     }
 }
