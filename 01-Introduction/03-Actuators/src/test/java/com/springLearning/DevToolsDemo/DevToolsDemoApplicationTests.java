e6649d9fa3cb5acc243e05dc956542437c6dac06
lokeswarareddyajjuguttu <lokeswarareddy.ajjuguttu49@gmail.com>
2026-01-31 20:50:46 +0530

    Actuators: Added actuators and spring security demo folder

---
+++ 01-Introduction/03-Actuators/src/test/java/com/springLearning/DevToolsDemo/DevToolsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springLearning.DevToolsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevToolsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
